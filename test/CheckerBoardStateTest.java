da7f38f555927f2a288b27aaaec08db17ca1d8df
Luke A Weaver <l501w882@ku.edu>
2016-12-14 17:50:34 -0600

    Update CheckerBoardStateTest.java
    Added Test 10-16

--- test/CheckerBoardStateTest.java
+++ test/CheckerBoardStateTest.java
@@ -1,7 +1,16 @@
 import com.groupone.p2pgame.*;
+import java.util.Arrays;
+
 
 public class CheckerBoardStateTest {
 
+
+
+      /**
+         Checks if creating a new board will create a board with out any
+         peices.
+         @return returns whether or not the test succeeded"
+      */
     public static boolean test1() {
         CheckerBoardState board = new CheckerBoardState();
         for (CheckerSquare square : board.getSquares()) {
@@ -11,7 +20,11 @@ public class CheckerBoardStateTest {
         }
         return true;
     }
-
+    /**
+       Checks if the pieces for player one are in their starting positions
+       at the beginning of a game.
+       @return returns whether or not the test succeeded"
+    */
     public static boolean test2() {
         CheckerBoardState board = CheckerBoardState.getStartingBoard();
         int i = 0;
@@ -30,6 +43,11 @@ public class CheckerBoardStateTest {
         return check;
     }
 
+    /**
+       Checks if the pieces for player two are in their starting positions
+       at the beginning of a game.
+       @return returns whether or not the test succeeded"
+    */
     public static boolean test3() {
         CheckerBoardState board = CheckerBoardState.getStartingBoard();
         int i = 0;
@@ -47,6 +65,12 @@ public class CheckerBoardStateTest {
           }
         return check;
     }
+
+    /**
+       Checks that spaces that should not have pieces on them at the beggining
+       of the game are indeed empty.
+       @return returns whether or not the test succeeded"
+    */
     public static boolean test4() {
         CheckerBoardState board = CheckerBoardState.getStartingBoard();
         int i = 0;
@@ -66,7 +90,11 @@ public class CheckerBoardStateTest {
           }
         return check;
     }
-
+    /**
+       Checks whether or not a pawn is able to be added to an already
+       existing board.
+       @return returns whether or not the test succeeded"
+    */
     public static boolean test5() {
         CheckerBoardState board = CheckerBoardState.getStartingBoard();
         Piece p1 = new Piece(PieceType.PAWN, Player.ONE);
@@ -83,7 +111,11 @@ public class CheckerBoardStateTest {
 
         return check;
     }
-
+    /**
+       Checks whether or not a king is able to be added to an already
+       existing board.
+       @return returns whether or not the test succeeded"
+    */
         public static boolean test6() {
             CheckerBoardState board = CheckerBoardState.getStartingBoard();
             Piece p1 = new Piece(PieceType.KING, Player.ONE);
@@ -100,7 +132,10 @@ public class CheckerBoardStateTest {
 
             return check;
         }
-
+        /**
+           Checks whether or not a king is able to be removed from the board.
+           @return returns whether or not the test succeeded"
+        */
         public static boolean test7() {
             CheckerBoardState board = CheckerBoardState.getStartingBoard();
             Piece p1 = new Piece(PieceType.KING, Player.ONE);
@@ -125,7 +160,10 @@ public class CheckerBoardStateTest {
             return check;
         }
 
-
+        /**
+           Checks whether or not a pawn is able to be removed from the board.
+           @return returns whether or not the test succeeded"
+        */
         public static boolean test8() {
             CheckerBoardState board = CheckerBoardState.getStartingBoard();
             Piece p1 = new Piece(PieceType.PAWN, Player.ONE);
@@ -149,6 +187,11 @@ public class CheckerBoardStateTest {
             }
             return check;
         }
+
+        /**
+           Checks if player two's pieces are able to be moved on the board.
+           @return returns whether or not the test succeeded"
+        */
         public static boolean test9() {
           CheckerBoardState board = CheckerBoardState.getStartingBoard();
           CheckerSquare original = board.getSquare(40);
@@ -165,6 +208,10 @@ public class CheckerBoardStateTest {
           return check;
         }
 
+        /**
+           Checks if player one's pieces are able to be moved on the board.
+           @return returns whether or not the test succeeded"
+        */
         public static boolean test10() {
           CheckerBoardState board = CheckerBoardState.getStartingBoard();
           CheckerSquare original = board.getSquare(19);
@@ -180,7 +227,135 @@ public class CheckerBoardStateTest {
           }
           return check;
         }
+        /**
+           Checks if the isDoubleJump method returns true if a double jump
+           is possible
+           @return returns whether or not the test succeeded"
+        */
+        public static boolean test11() {
+          boolean check = false;
+          CheckerBoardState board = CheckerBoardState.getStartingBoard();
 
+
+          CheckerSquare testPosition[] = board.getSquares();
+          Piece empty = new Piece(PieceType.EMPTY, Player.NONE);
+
+          testPosition[14].setPiece(empty);
+
+          Piece p2 = new Piece(PieceType.PAWN, Player.TWO);
+          testPosition[35].setPiece(p2);
+          CheckerMove doublejump = new CheckerMove(board.getSquare(42),board.getSquare(28));
+
+        //  System.out.println(testPosition[33].getPiece().getPlayer());
+          if(doublejump.isDoubleJump())
+          {
+            check = true;
+          }
+          return check;
+        }
+
+        /**
+           Checks if the isSingleJump method returns true if a single jump
+           is possible
+           @return returns whether or not the test succeeded"
+        */
+        public static boolean test12() {
+          boolean check = false;
+          CheckerBoardState board = CheckerBoardState.getStartingBoard();
+
+
+          CheckerSquare testPosition[] = board.getSquares();
+          Piece empty = new Piece(PieceType.EMPTY, Player.NONE);
+
+          testPosition[14].setPiece(empty);
+
+          Piece p2 = new Piece(PieceType.PAWN, Player.TWO);
+          testPosition[35].setPiece(p2);
+          CheckerMove doublejump = new CheckerMove(board.getSquare(42),board.getSquare(35));
+
+        //  System.out.println(testPosition[33].getPiece().getPlayer());
+          if(doublejump.isSingleJump())
+          {
+            check = true;
+          }
+          return check;
+        }
+
+        /**
+           Checks if the addPieceAtIndex method adds a piece at a specified
+           index.
+           @return returns whether or not the test succeeded"
+        */
+        public static boolean test13() {
+            boolean check = false;
+            CheckerBoardState board = CheckerBoardState.getStartingBoard();
+            Piece p1 = new Piece(PieceType.PAWN, Player.ONE);
+            board.addPieceAtIndex(p1,35);
+            CheckerSquare testPosition[] = board.getSquares();
+
+            testPosition[0].setPiece(p1);
+          //  System.out.println(testPosition[0].getPiece().getPlayer());
+
+            if(testPosition[35].getPiece().getType() == PieceType.PAWN && testPosition[35].getPiece().getPlayer() == Player.ONE)
+            {
+              check=true;
+            }
+
+            return check;
+        }
+
+        /**
+           Checks if getPlayerOnePieceLocationsInts returns the correct
+           index of player one's current pieces.
+           @return returns whether or not the test succeeded"
+        */
+        public static boolean test14() {
+            boolean check = false;
+            CheckerBoardState board = CheckerBoardState.getStartingBoard();
+            int[] test = board.getPlayerOnePieceLocationsInts();
+            int[] playerOneLocations = new int[] {1, 3, 5, 7, 8, 10, 12, 14, 17, 19, 21, 23};
+            CheckerSquare testPosition[] = board.getSquares();
+            if(Arrays.equals(test,playerOneLocations))
+            {
+              check=true;
+            }
+            return check;
+        }
+
+        /**
+           Checks if getPlayerTwoPieceLocationsInts returns the correct
+           index of player two's current pieces.
+           @return returns whether or not the test succeeded"
+        */
+        public static boolean test15() {
+            boolean check = false;
+            CheckerBoardState board = CheckerBoardState.getStartingBoard();
+            int[] test = board.getPlayerTwoPieceLocationsInts();
+            int[] playerTwoLocations = new int[] {40, 42, 44, 46, 49, 51, 53, 55, 56, 58, 60, 62};
+            CheckerSquare testPosition[] = board.getSquares();
+            if(Arrays.equals(test,playerTwoLocations))
+            {
+              check=true;
+            }
+            return check;
+        }
+
+        /**
+           Checks if isValidMove returns true when a move is valid.
+           @return returns whether or not the test succeeded"
+        */
+        public static boolean test16() {
+            boolean check = false;
+            CheckerBoardState board = CheckerBoardState.getStartingBoard();
+            CheckerMove validMove = new CheckerMove(board.getSquare(21),board.getSquare(28));
+            CheckerSquare testPosition[] = board.getSquares();
+
+            if(board.isValidMove(validMove))
+            {
+              check=true;
+            }
+            return check;
+        }
 
 
     public static void main(String args[]) {
@@ -237,7 +412,40 @@ public class CheckerBoardStateTest {
         if (test10()) {
             System.out.println("test 10: Able to move P1 piece on board.. succeeded");
         } else {
-            System.out.println("test 10: AAble to move P1 piece on board.. failed");
+            System.out.println("test 10: Able to move P1 piece on board.. failed");
         }
+        if (test11()) {
+            System.out.println("test 11: isDoubleJump method returns true when double jump is possible.. succeeded");
+        } else {
+            System.out.println("test 11:  isDoubleJump method returns true when double jump is possible.. failed");
+        }
+
+        if (test12()) {
+            System.out.println("test 12: isSingleJump method returns true when signle jump is possible.. succeeded");
+        } else {
+            System.out.println("test 12: isSingleJump method returns true when signle jump is possible.. failed");
+        }
+
+        if (test13()) {
+            System.out.println("test 13: addPieceAtIndex method adds piece to index.. succeeded");
+        } else {
+            System.out.println("test 13: addPieceAtIndex method adds piece to index.. failed");
+        }
+        if (test14()) {
+            System.out.println("test 14: getPlayerOnePieceLocationsInts method returns player ones piece locations.. succeeded");
+        } else {
+            System.out.println("test 14: getPlayerOnePieceLocationsInts method returns player ones piece locations.. failed");
+        }
+        if (test15()) {
+            System.out.println("test 15: getPlayerTwoPieceLocationsInts method returns player ones piece locations.. succeeded");
+        } else {
+            System.out.println("test 15: getPlayerTwoPieceLocationsInts method returns player ones piece locations.. failed");
+        }
+        if (test16()) {
+            System.out.println("test 16: isValidmove method checks if the move is valid.. succeeded");
+        } else {
+            System.out.println("test 16: isValidmove method checks if the move is valid.. failed");
+        }
+        
     }
 }
